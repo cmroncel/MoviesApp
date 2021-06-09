b0e56e3ee8dad7587a337512eb33573abc583a2b
Cemre ÖNCEL <cmroncel@gmail.com>
2021-06-10 00:42:51 +0300

    Add moya lib, repositories & services

--- MoviesApp/AppDelegate.swift
+++ MoviesApp/AppDelegate.swift
@@ -9,28 +9,34 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    static let shared: AppDelegate = UIApplication.shared.delegate as! AppDelegate
 
-
+    var window: UIWindow?
+    lazy var rootNavigationController: UINavigationController = window?.rootViewController as! UINavigationController
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Set root vc
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = getRootViewController()
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .white
+        
         return true
     }
+}
 
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+// MARK:- Root VC Provider
+extension AppDelegate {
+    private func getRootViewController() -> UIViewController {
+        let launchVC: MainViewController = MainViewController.create()
+        let navController = UINavigationController(rootViewController: launchVC)
+        navController.navigationBar.isHidden = true
+        navController.interactivePopGestureRecognizer?.isEnabled = false
+        navController.interactivePopGestureRecognizer?.delegate = nil
+        navController.view.backgroundColor = .white
+        return navController
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 

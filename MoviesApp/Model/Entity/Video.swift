3f44f7158774d653b294efc026b360c372dd30c3
Cemre ÖNCEL <cmroncel@gmail.com>
2021-06-12 02:23:36 +0300

    Add Video model

---
+++ MoviesApp/Model/Entity/Video.swift
@@ -0,0 +1,15 @@
+//
+//  Video.swift
+//  MoviesApp
+//
+//  Created by Cemre Öncel on 12.06.2021.
+//
+
+import Foundation
+
+struct Video: Codable {
+    let id: String?
+    let key: String?
+    let name: String?
+    let site: String?
+}

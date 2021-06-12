07e68228470e16d7a256bed70325066362e29a68
Cemre ÖNCEL <cmroncel@gmail.com>
2021-06-12 19:20:18 +0300

    Add PersonItem model

---
+++ MoviesApp/Model/Entity/PersonItem.swift
@@ -0,0 +1,14 @@
+//
+//  PersonItem.swift
+//  MoviesApp
+//
+//  Created by Cemre Öncel on 12.06.2021.
+//
+
+import Foundation
+
+struct PersonItem: Codable {
+    let id: Int?
+    let profile_path: String?
+    let name: String?
+}
